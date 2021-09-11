fe19f908950fcd308910a6844fb43221120cd7a0
kalilask4 <kalilask4@gmail.com>
2021-09-11 15:14:25 +0300

    add entities for transaction

---
+++ portfolio/portfolio.Domain/Entities/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.Domain.Entities
+{
+    abstract class Transaction
+    {
+        public string TransactionId { get; set; }
+        public string Side { get; set; }
+        public string Symbol { get; set; }
+        public Coin DebetCoin { get; set; }
+        public Coin CreditCoin { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Prise { get; set; }
+        public DateTime DateAdd { get; set; }
+
+        protected Transaction()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Transaction {this.Side}: {this.Symbol}  {this.Amount}  {this.Prise} Total: {this.Amount}*{this.Prise}";
+        }
+
+    }
+}
