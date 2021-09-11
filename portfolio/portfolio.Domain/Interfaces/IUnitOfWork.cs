0c727a2f9036436f4fe25cba6af9fc26a2207aae
kalilask4 <kalilask4@gmail.com>
2021-09-11 15:54:51 +0300

    pattern Unit of Work: add interface IUnitofWork

---
+++ portfolio/portfolio.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using portfolio.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.Domain.Interfaces
+{
+    interface IUnitOfWork
+    {
+        IRepository<Coin> CoinRepository { get; }
+        IRepository<Buying> BuyingRepository { get; }
+        IRepository<Selling> SelliRepository { get; }
+
+        void SaveChanges();
+    }
+}
