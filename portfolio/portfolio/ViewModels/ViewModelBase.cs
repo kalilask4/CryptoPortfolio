48bbd94fb8ce7f747aa8db5a5f4392184134182d
kalilask4 <kalilask4@gmail.com>
2021-09-16 18:49:07 +0300

    add ViewModelBase (use INotifyPropertyChanged to bind the markup and model data two-way)

---
+++ portfolio/portfolio/ViewModels/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.ViewModels
+{
+    //In order to be able to bind the markup and model data two-way
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string
+propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new
+           PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Метод изменения свойства с вызовом события PropertyChanged
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="prop">property for changing</param>
+        /// <param name="value">new value</param>
+        /// <param name="propName">property name</param>
+        /// <returns></returns>
+        protected bool Set<T>(ref T prop, T value, [CallerMemberName]
+string propName=null)
+        {
+            if (Equals(prop, value)) return false;
+            prop = value;
+            OnPropertyChanged(propName);
+            return true;
+        }
+    }
+}
