6713509822514591b7defe3dd300904abc8b9176
kalilask4 <kallilask4@gmail.com>
2021-09-26 15:59:12 +0300

    change Transaction: properties DebetCoin and CreditCoin  replased by dictionary transaction Coins

--- portfolio/portfolio.Domain/Entities/Transaction.cs
+++ portfolio/portfolio.Domain/Entities/Transaction.cs
@@ -10,22 +10,36 @@ namespace portfolio.Domain.Entities
     {
         public int TransactionId { get; set; }
         public string Side { get; set; }
-        public Coin DebetCoin { get; set; }
-        public Coin CreditCoin { get; set; }
+        public Dictionary<String, Coin> transactionCoins { get; set; }
+
+        //public Coin DebetCoin { get; set; }
+        //public Coin CreditCoin { get; set; }
         public decimal Amount { get; set; }
-        public decimal Prise { get; set; }
-        public DateTime TradeDate { get; set; }
+        public decimal Priсe { get; set; }
+        public DateTime AddDate { get; set; }
         public string Symbol { get; set; }
 
         public Transaction()
         {
-            Symbol = "symbol";
-                //DebetCoin.Symbol + CreditCoin.Symbol;
+            transactionCoins = new Dictionary<string, Coin>();
+            AddDate = DateTime.Now;
+        }
+
+        public Transaction(Coin debetCoin, Coin creditCoin, decimal amount, decimal priсe)
+        {
+            transactionCoins = new Dictionary<string, Coin>(2);
+            transactionCoins.Add("debet", debetCoin);
+            transactionCoins.Add("credit", creditCoin);
+            Amount = amount;
+            Priсe = priсe;
+            AddDate = DateTime.Now;
+            Symbol = debetCoin.CoinName + creditCoin.CoinName;
+            //"default_symbol"; //DebetCoin.Symbol + CreditCoin.Symbol;
         }
 
         public override string ToString()
         {
-            return $"Transaction {this.Side}: {this.Symbol}  {this.Amount}  {this.Prise} Total: {this.Amount}*{this.Prise}";
+            return $"Transaction {this.Side}: {this.Symbol}  {this.Amount}  {this.Priсe} Total: {this.Amount}*{this.Priсe}";
         }
     }
 }
