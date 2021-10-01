89fd72fcc5ff078d382cc50fb0a1716ac2498054
kalilask4 <kalilask4@gmail.com>
2021-10-01 19:02:22 +0300

    add PortfolioContext

---
+++ portfolio/portfolio.DAL/Data/PortfolioContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using portfolio.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.DAL.Data
+{
+    class PortfolioContext : DbContext
+    {
+        public PortfolioContext(DbContextOptions<PortfolioContext> options) : base(options)
+        { }
+
+        public DbSet<Coin> Coins { get; set; }
+        public DbSet<BuyTransaction> buyTransactions { get; set; }
+        public DbSet<SellTransaction> sellTransactions { get; set; }
+        
+    }
+}
