b081e96a846142f2600f04a4459165c3cfe41c5d
kalilask4 <kalilask4@gmail.com>
2021-10-16 14:46:35 +0300

    work with Domain-level. Add models for entities: Coin, Transaction

--- portfolio/portfolio.Domain/Entities/Coin.cs
+++ portfolio/portfolio.Domain/Entities/Coin.cs
@@ -9,6 +9,21 @@ namespace portfolio.Domain.Entities
 {
     public class Coin
     {
-        
+        [Key]
+        public int CoinId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Symbol { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public decimal CurrentPrice { get; set; }
+        public decimal ValueUSD { get; set; }
+        public decimal AveragePurchasePrice { get; set; }
+        public string PictureName { get; set; }
+        public DateTime DateUpdate { get; set; }
+        // навигационное свойство
+        public List<Transaction> Transactions { get; set; }
     }
 }
--- portfolio/portfolio.Domain/Entities/Transaction.cs
+++ portfolio/portfolio.Domain/Entities/Transaction.cs
@@ -6,7 +6,28 @@ namespace portfolio.Domain.Entities
 {
     public class Transaction
     {
+        [Key]
+        public int TransactionId { get; set; }
+        [Required]
+        string side;
+        static List<string> SideType = new List<string> { "buy", "sell", "transfer" };
+        public string Side
+        {
+            get { return side; }
+            set
+            {
+                if (SideType.Contains(value))
+                    side = value;
+            }
+        }
 
+        public string TransactionSymbol { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Priсe { get; set; }
+        public decimal Sum { get; set; }
+        public DateTime DateUpdate { get; set; }
+        // навигационное свойство
+        public List<Coin> TransactionCoins { get; set; }
 
     }
 }
