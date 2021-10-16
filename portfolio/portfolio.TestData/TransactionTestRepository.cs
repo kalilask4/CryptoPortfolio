2fae7bdc1e11fdadead2df03bf3ec46f588ef974
kalilask4 <kalilask4@gmail.com>
2021-10-16 17:14:43 +0300

    add TransactionTestRepository

---
+++ portfolio/portfolio.TestData/TransactionTestRepository.cs
@@ -0,0 +1,44 @@
+﻿using portfolio.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace portfolio.TestData
+{
+    public class TransactionTestRepository : IRepository<Transaction>
+    {
+        public void Create(Transaction entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Transaction> Find(Expression<Func<Transaction, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaction Get(int id, params string[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Transaction> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Transaction entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
