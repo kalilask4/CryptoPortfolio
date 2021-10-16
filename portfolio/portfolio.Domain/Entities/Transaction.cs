23bcacffe12fc986be9f4aaa84d69cfc3987b573
kalilask4 <kalilask4@gmail.com>
2021-10-16 15:38:59 +0300

    add constructors for Coin and Transaction

--- portfolio/portfolio.Domain/Entities/Transaction.cs
+++ portfolio/portfolio.Domain/Entities/Transaction.cs
@@ -11,7 +11,15 @@ namespace portfolio.Domain.Entities
         public string Symbol { get; set; } //like Name exs BTNBTC
         [Required]
         private string side;
-        static List<string> SideType = new List<string> { "buy", "sell", "transfer" };
+        static readonly List<string> SideType = new List<string> { "buy", "sell", "transfer" };
+        
+        public decimal Amount { get; set; }
+        public decimal Priсe { get; set; }
+        public decimal Sum { get; set; }
+        public DateTime DateUpdate { get; set; }
+        // навигационное свойство
+        public List<Coin> TransactionCoins { get; set; }
+        
         public string Side
         {
             get { return side; }
@@ -22,14 +30,15 @@ namespace portfolio.Domain.Entities
             }
         }
 
-        
-        public decimal Amount { get; set; }
-        public decimal Priсe { get; set; }
-        public decimal Sum { get; set; }
-        public DateTime DateUpdate { get; set; }
-        // навигационное свойство
-        public List<Coin> TransactionCoins { get; set; }
-
+        public Transaction()
+        {
+            Symbol = "DefaultSymbol";
+            Side = "buy";
+            Amount = 0;
+            Priсe = 0;
+            DateUpdate = DateTime.Now; 
+            TransactionCoins = new List<Coin>(2);
+        }
 
         public override string ToString()
         {
