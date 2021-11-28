bfe587d7a0b166b919683ac1e095ac437e0ac336
kalilask4 <kalilask4@gmail.com>
2021-11-29 01:22:33 +0300

    Add PortfolioIndicator

---
+++ portfolio/portfolio.Business/PortfolioPerfomance.cs
@@ -0,0 +1,8 @@
+﻿namespace portfolio.Business
+{
+    public class PortfolioIndicator
+    {
+        public  string IndicatorName { get; set; }
+        public decimal Value { get; set; }
+    }
+}
